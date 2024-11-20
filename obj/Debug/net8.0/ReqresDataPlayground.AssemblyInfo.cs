e60acccfd424da2c51bf5c42faa1300977a99a18
HilmiSozer <hilmi5282@hotmail.com>
2024-11-20 12:59:37 +0300

    Get,Post,Delete

--- obj/Debug/net8.0/ReqresDataPlayground.AssemblyInfo.cs
+++ obj/Debug/net8.0/ReqresDataPlayground.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReqresDataPlayground")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+533a5369a3c73619f105bcc633722ba2309a20d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReqresDataPlayground")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReqresDataPlayground")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
